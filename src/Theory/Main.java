a93fd965c859456fcddee3e6ea9a4d3a7e1da29a
Sviirina <vozilkina@inbox.ru>
2022-11-15 06:48:35 +0700

    Theory part

---
+++ src/Theory/Main.java
@@ -0,0 +1,12 @@
+package Theory;
+
+public class Main {
+    static int square (int a){
+        return a*a;
+    }
+
+    public static void main(String[] args){
+        System.out.println(square(7));
+
+    }
+}
